2981a67f25bd651e2eb0b7115e67bfbb8904e068
Ekaterina <katepthv12@gmail.com>
2023-01-19 21:42:52 +0100

    add active slide in js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
+  slides[activeSlide].classList.add("active");
 
-    })
-}
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
 
-function clearActiveClasses() {
+  function clearActiveClasses() {
     slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
+      slide.classList.remove("active");
+    });
+  }
 }
+
+slidesPlugin()
